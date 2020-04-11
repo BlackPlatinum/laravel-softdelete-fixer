f0d98ee7a530c02989c028924e0915103c5b7828
AshkanAbd <kotksthegame@gmail.com>
2020-04-11 05:07:37 +0430

    Try to find table's model class.

--- BlackPlatinum/Builder.php
+++ BlackPlatinum/Builder.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace BlackPlatinum;
 
+namespace App\Component\Extensions;
+
+
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Builder extends \Illuminate\Database\Eloquent\Builder
@@ -18,6 +21,100 @@ class Builder extends \Illuminate\Database\Eloquent\Builder
     }
 
     /**
+     * Get Model class by it's name
+     *
+     * @param string $modelName
+     * @return string
+     */
+    protected function getModelClass(string $modelName): string
+    {
+        $fromEnv = $this->getModelClassFromEnv($modelName);
+        if ($fromEnv) return $fromEnv;
+
+        $fromCurrentModel = $this->getModelClassFormCurrentModel($modelName);
+        if ($fromCurrentModel) return $fromCurrentModel;
+
+        $fromNamespace = $this->getModelClassFromAppNamespace($modelName);
+        if ($fromNamespace) return $fromNamespace;
+
+        $fromSearch = $this->getModelClassFromSearching($modelName);
+        if ($fromSearch) return $fromSearch;
+
+        return false;
+    }
+
+    /**
+     * Last way is searching in declared class to find table's model class
+     *
+     * @param string $modelName
+     * @return bool|mixed
+     */
+    protected function getModelClassFromSearching(string $modelName)
+    {
+        foreach (get_declared_classes() as $class) {
+            if (is_subclass_of($class, 'Illuminate\Database\Eloquent\Model')) {
+                if (class_basename($class) === $modelName) {
+                    return $class;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Try to get model from app namespace
+     *
+     * @param string $modelsName
+     * @return bool|string
+     */
+    protected function getModelClassFromAppNamespace(string $modelsName)
+    {
+        if (!class_exists(app()->getNamespace() . $modelsName)) return false;
+        return app()->getNamespace() . $modelsName;
+    }
+
+    /**
+     * Try to get model from current query model's namespace
+     *
+     * @param string $modelName
+     * @return bool|string
+     */
+    protected function getModelClassFormCurrentModel(string $modelName)
+    {
+        $thisClass = get_class($this->model);
+        $namespace = str_replace(class_basename($thisClass), '', $thisClass);
+        if (!class_exists($namespace . $modelName)) return false;
+        return $namespace . $modelName;
+    }
+
+    /**
+     * Try to get model from environment key
+     *
+     * @param string $modelName
+     * @return bool|string
+     */
+    protected function getModelClassFromEnv(string $modelName)
+    {
+        $namespace = env('MODEL_NAMESPACE', false);
+        if (!$namespace) return false;
+        if (!class_exists("$namespace\\$modelName")) return false;
+        return "$namespace\\$modelName";
+    }
+
+    /**
+     * Check given table has soft delete or not
+     *
+     * @param string $table
+     * @return bool
+     */
+    protected function checkSoftDelete(string $table): bool
+    {
+        $model = $this->getModelName($table);
+        $model = $this->getModelClass($model);
+        return in_array(SoftDeletes::class, class_uses_recursive($model));
+    }
+
+    /**
      * Add a join clause to the query.
      *
      * @param string $table
@@ -26,11 +123,18 @@ class Builder extends \Illuminate\Database\Eloquent\Builder
      * @param string|null $second
      * @param string $type
      * @param bool $where
+     * @param bool $withTrash
      * @return \Illuminate\Database\Query\Builder|$this|\Illuminate\Database\Eloquent\Builder
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    public function join($table, $first, $operator = null, $second = null, $type = 'inner', $where = false)
+    public function join($table, $first, $operator = null, $second = null, $type = 'inner', $where = false, $withTrash = false)
     {
-        return parent::join($table, $first, $operator, $second, $type, $where)
-            ->where("$table.deleted_at", NULL);
+        if (!$withTrash) {
+            if ($this->checkSoftDelete($table)) {
+                return parent::join($table, $first, $operator, $second, $type, $where)
+                    ->where("$table.deleted_at", NULL);
+            }
+        }
+        return parent::join($table, $first, $operator, $second, $type, $where);
     }
 }
--- BlackPlatinum/SoftDeletes.php
+++ BlackPlatinum/SoftDeletes.php
@@ -1,11 +1,17 @@
 <?php
 
-namespace BlackPlatinum;
 
-trait SoftDeletes
+namespace App\Component\Extensions;
+
+
+trait SoftDeletesFix
 {
-    use \Illuminate\Database\Eloquent\SoftDeletes;
-
+    /**
+     * Create a new Eloquent query builder for the model.
+     *
+     * @param \Illuminate\Database\Query\Builder $query
+     * @return Builder
+     */
     public function newEloquentBuilder($query)
     {
         return new Builder($query);
